286e2820eabe7d8be7853b06a4c66cc6ed5d2d67
Áron Kósa <kosaa@kkszki.hu>
2025-12-10 10:54:24 +0100

    Adatok betöltése, Hozzáad függvény

--- WpfDolgozat/MainWindow.xaml.cs
+++ WpfDolgozat/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,19 +15,57 @@ namespace WpfDolgozat
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public class Dolgozat
+    {
+        public string Nev { get; set;}
+        public int Eletkor { get; set;}
+        public int Pontszam { get; set;}
+
+        public Dolgozat(string nev, int eletkor, int pontszam)
+        {
+            Nev = nev;
+            Eletkor = eletkor;
+            Pontszam = pontszam;
+        }
+    }
     public partial class MainWindow : Window
     {
+        public List <Dolgozat> dolgozatok=new List<Dolgozat> ();
         public MainWindow()
         {
             InitializeComponent();
+            var sorok=File.ReadAllLines("dolgozatok.txt").Skip(1);
+            foreach(var s in sorok)
+            {
+                string[] darabok = s.Split(';');
+                string neve = darabok[0];
+                int eletkora = Convert.ToInt32(darabok[1]);
+                int pontszam = Convert.ToInt32(darabok[2]);
+                dolgozatok.Add(new Dolgozat(neve,eletkora,pontszam));
+            }
+            dataGrid.ItemsSource = dolgozatok;
         }
 
         private void Hozzaadas(object sender, RoutedEventArgs e)
         {
-
+            int eletkora;
+            int pontszama;
+            if(nevInput.Text.Length > 0 && int.TryParse(eletkorInput.Text,out eletkora) && eletkora >6&& int.TryParse(pontszamInput.Text, out pontszama) && pontszama < 101)
+            {
+                dolgozatok.Add(new Dolgozat(nevInput.Text, eletkora, pontszama));
+                dataGrid.ItemsSource= dolgozatok;
+                dataGrid.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Nem megfelelőek a beviteli adatok!");
+            }
         }
 
-        private void Torles(object sender, RoutedEventArgs e)
+        
+
+        private void Mentes(object sender, RoutedEventArgs e)
         {
 
         }
